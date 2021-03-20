8f71cc625fac6b4417b123b0f4db1033196dd1e5
iBenatar-96 <iBenatar-96@users.noreply.github.com>
2021-03-20 19:06:34 +0200

    New files

--- background.js
+++ background.js
@@ -1,1 +1,18 @@
-alert("Hi")
+
+function mySource(info, tab){
+    console.log("Clicked on page:", info, tab)
+}
+
+function myImageClick(info,tab){
+    console.log("Clicked an image",info,tab)
+}
+chrome.contextMenus.create({
+    "title": "Source",
+    "contexts": ["page"],
+    "onclick": mySource
+})
+chrome.contextMenus.create({
+    "title": "Share image",
+    "contexts": ["image"],
+    "onclick": myImageClick
+})
